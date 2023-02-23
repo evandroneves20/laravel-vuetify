eb34bd5c44439f26bb6f546b2506e5172b2a0e62
Evandro <evandroneves@hotmail.com>
2023-02-23 14:29:17 -0300

    atualizacao layout principal

--- public/js/resources_js_layout_MainLayout_vue.js
+++ public/js/resources_js_layout_MainLayout_vue.js
@@ -26,7 +26,7 @@ __webpack_require__.r(__webpack_exports__);
         title: 'Users',
         icon: 'mdi-account-group-outline'
       }],
-      rail: true
+      rail: false
     };
   }
 });
@@ -45,132 +45,230 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("b", null, "Fulano da Silva", -1 /* HOISTED */);
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1 /* HOISTED */);
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("small", null, "Administrador", -1 /* HOISTED */);
+
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  var _component_v_btn = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-btn");
+  var _component_v_img = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-img");
+  var _component_v_col = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-col");
+  var _component_v_row = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-row");
+  var _component_v_container = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-container");
   var _component_v_list_item = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-list-item");
-  var _component_v_divider = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-divider");
   var _component_v_list = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-list");
   var _component_v_navigation_drawer = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-navigation-drawer");
+  var _component_v_theme_provider = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-theme-provider");
   var _component_v_app_bar_nav_icon = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-app-bar-nav-icon");
-  var _component_v_toolbar_title = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-toolbar-title");
+  var _component_v_app_bar_title = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-app-bar-title");
   var _component_v_spacer = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-spacer");
+  var _component_v_icon = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-icon");
+  var _component_v_btn = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-btn");
   var _component_v_app_bar = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-app-bar");
   var _component_v_breadcrumbs = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-breadcrumbs");
   var _component_v_card = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-card");
-  var _component_v_card_text = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-card-text");
   var _component_v_main = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-main");
   var _component_v_layout = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("v-layout");
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_v_card, null, {
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
       return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_layout, null, {
         "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_navigation_drawer, {
-            modelValue: $data.drawer,
-            "onUpdate:modelValue": _cache[1] || (_cache[1] = function ($event) {
-              return $data.drawer = $event;
-            }),
-            rail: $data.rail,
-            permanent: "",
-            onClick: _cache[2] || (_cache[2] = function ($event) {
-              return $data.rail = false;
-            })
+          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_theme_provider, {
+            theme: "dark",
+            "with-background": ""
           }, {
             "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
-                "prepend-avatar": "https://randomuser.me/api/portraits/men/85.jpg",
-                title: "John Leider",
-                nav: ""
-              }, {
-                append: (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_btn, {
-                    variant: "text",
-                    icon: "mdi-chevron-left",
-                    onClick: _cache[0] || (_cache[0] = (0,vue__WEBPACK_IMPORTED_MODULE_0__.withModifiers)(function ($event) {
-                      return $data.rail = !$data.rail;
-                    }, ["stop"]))
-                  })];
+              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_navigation_drawer, {
+                modelValue: $data.drawer,
+                "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
+                  return $data.drawer = $event;
                 }),
-                _: 1 /* STABLE */
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_divider), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list, {
-                density: "compact",
-                nav: ""
+                rail: $data.rail,
+                permanent: "",
+                onClick: _cache[1] || (_cache[1] = function ($event) {
+                  return $data.rail = false;
+                }),
+                width: "220"
               }, {
                 "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
-                    "prepend-icon": "mdi-home-city",
-                    title: "Home",
-                    value: "home"
-                  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
-                    "prepend-icon": "mdi-account",
-                    title: "My Account",
-                    value: "account"
-                  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
-                    "prepend-icon": "mdi-account-group-outline",
-                    title: "Users",
-                    value: "users"
-                  })];
-                }),
-                _: 1 /* STABLE */
-              })];
-            }),
+                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_container, null, {
+                    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_row, {
+                        align: "center",
+                        "no-gutters": "",
+                        style: (0,vue__WEBPACK_IMPORTED_MODULE_0__.normalizeStyle)($data.rail === false ? 'height: 90px;' : '')
+                      }, {
+                        "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_col, {
+                            cols: "12",
+                            align: "center"
+                          }, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_img, {
+                                width: 100,
+                                "aspect-ratio": "16/9",
+                                cover: "",
+                                src: "https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                              })];
+                            }),
+                            _: 1 /* STABLE */
+                          })];
+                        }),
 
-            _: 1 /* STABLE */
-          }, 8 /* PROPS */, ["modelValue", "rail"]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_app_bar, {
-            color: "primary",
-            prominent: ""
-          }, {
-            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_app_bar_nav_icon, {
-                variant: "text",
-                onClick: _cache[3] || (_cache[3] = (0,vue__WEBPACK_IMPORTED_MODULE_0__.withModifiers)(function ($event) {
-                  return $data.drawer = !$data.drawer;
-                }, ["stop"]))
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_toolbar_title, null, {
-                "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("My files")];
+                        _: 1 /* STABLE */
+                      }, 8 /* PROPS */, ["style"]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_row, {
+                        align: "center",
+                        "no-gutters": ""
+                      }, {
+                        "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_col, {
+                            cols: "12",
+                            align: "center",
+                            "class": (0,vue__WEBPACK_IMPORTED_MODULE_0__.normalizeClass)($data.rail ? 'd-none' : '')
+                          }, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [_hoisted_1, _hoisted_2, _hoisted_3];
+                            }),
+                            _: 1 /* STABLE */
+                          }, 8 /* PROPS */, ["class"])];
+                        }),
+                        _: 1 /* STABLE */
+                      })];
+                    }),
+
+                    _: 1 /* STABLE */
+                  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list, {
+                    density: "compact",
+                    nav: ""
+                  }, {
+                    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
+                        "prepend-icon": "mdi-home-city",
+                        title: "Home",
+                        value: "home",
+                        active: ""
+                      }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
+                        "prepend-icon": "mdi-account",
+                        title: "My Account",
+                        value: "account"
+                      }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_list_item, {
+                        "prepend-icon": "mdi-account-group-outline",
+                        title: "Users",
+                        value: "users"
+                      })];
+                    }),
+                    _: 1 /* STABLE */
+                  })];
                 }),
+
                 _: 1 /* STABLE */
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_spacer), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_btn, {
-                variant: "text",
-                icon: "mdi-magnify"
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_btn, {
-                variant: "text",
-                icon: "mdi-filter"
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_btn, {
-                variant: "text",
-                icon: "mdi-dots-vertical"
-              })];
+              }, 8 /* PROPS */, ["modelValue", "rail"])];
             }),
             _: 1 /* STABLE */
-          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_main, {
-            style: {
-              "height": "95vh"
-            }
-          }, {
+          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_main, null, {
             "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
               return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_card, {
-                title: "Página de exemplo",
-                color: "yellow",
-                height: "80"
+                color: "grey-lighten-3"
               }, {
                 "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_breadcrumbs, {
-                    items: ['Foo', 'Bar', 'Fizz'],
-                    style: {
-                      "padding-top": "0"
-                    }
+                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_layout, null, {
+                    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_app_bar, {
+                        color: "teal-darken-4",
+                        image: "https://picsum.photos/1920/1080?random"
+                      }, {
+                        image: (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_img, {
+                            gradient: "to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+                          })];
+                        }),
+                        prepend: (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_app_bar_nav_icon, {
+                            onClick: _cache[2] || (_cache[2] = (0,vue__WEBPACK_IMPORTED_MODULE_0__.withModifiers)(function ($event) {
+                              return $data.rail = !$data.rail;
+                            }, ["stop"]))
+                          })];
+                        }),
+                        "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_app_bar_title, null, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Title")];
+                            }),
+                            _: 1 /* STABLE */
+                          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_spacer), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_btn, {
+                            icon: ""
+                          }, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_icon, null, {
+                                "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("mdi-power")];
+                                }),
+                                _: 1 /* STABLE */
+                              })];
+                            }),
+
+                            _: 1 /* STABLE */
+                          })];
+                        }),
+
+                        _: 1 /* STABLE */
+                      }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_main, null, {
+                        "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                          return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_card, {
+                            title: "Página de exemplo",
+                            color: "white",
+                            height: "80",
+                            rounded: 0
+                          }, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_breadcrumbs, {
+                                items: ['Foo', 'Bar', 'Fizz'],
+                                style: {
+                                  "padding-top": "0"
+                                }
+                              })];
+                            }),
+                            _: 1 /* STABLE */
+                          }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_container, {
+                            fluid: ""
+                          }, {
+                            "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                              return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_row, {
+                                dense: ""
+                              }, {
+                                "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                                  return [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)(4, function (n) {
+                                    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_col, {
+                                      key: n,
+                                      cols: "12"
+                                    }, {
+                                      "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+                                        return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_card, {
+                                          title: "Content ".concat(n),
+                                          subtitle: "Subtitle for Content ".concat(n),
+                                          text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+                                        }, null, 8 /* PROPS */, ["title", "subtitle"])];
+                                      }),
+                                      _: 2 /* DYNAMIC */
+                                    }, 1024 /* DYNAMIC_SLOTS */);
+                                  }), 64 /* STABLE_FRAGMENT */))];
+                                }),
+
+                                _: 1 /* STABLE */
+                              })];
+                            }),
+
+                            _: 1 /* STABLE */
+                          })];
+                        }),
+
+                        _: 1 /* STABLE */
+                      })];
+                    }),
+
+                    _: 1 /* STABLE */
                   })];
                 }),
-                _: 1 /* STABLE */
-              }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_v_card_text, {
-                style: {
-                  "padding": "20px",
-                  "background-color": "antiquewhite"
-                }
-              }, {
-                "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-                  return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" The navigation drawer will appear from the bottom on smaller size screens. ")];
-                }),
+
                 _: 1 /* STABLE */
               })];
             }),
--- resources/js/layout/MainLayout.vue
+++ resources/js/layout/MainLayout.vue
@@ -1,57 +1,95 @@
 <template>
     <v-card>
         <v-layout>
-            <v-navigation-drawer
-                v-model="drawer"
-                :rail="rail"
-                permanent
-                @click="rail = false"
-            >
-                <v-list-item
-                    prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-                    title="John Leider"
-                    nav
+            <v-theme-provider theme="dark" with-background>
+                <v-navigation-drawer
+                    v-model="drawer"
+                    :rail="rail"
+                    permanent
+                    @click="rail = false"
+                    width="220"
                 >
-                    <template v-slot:append>
-                        <v-btn
-                            variant="text"
-                            icon="mdi-chevron-left"
-                            @click.stop="rail = !rail"
-                        ></v-btn>
-                    </template>
-                </v-list-item>
+                    <v-container>
+                        <v-row
+                            align="center"
+                            no-gutters
+                            :style="rail === false ? 'height: 90px;' : ''"
+                        >
+                            <v-col cols="12" align="center">
+                                <v-img
+                                    :width="100"
+                                    aspect-ratio="16/9"
+                                    cover
+                                    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                                ></v-img>
+                            </v-col>
+                        </v-row>
+                        <v-row
+                            align="center"
+                            no-gutters
+                        >
+                            <v-col cols="12" align="center" :class="rail ? 'd-none' : ''">
+                                <b>Fulano da Silva</b><br/>
+                                <small>Administrador</small>
+                            </v-col>
+                        </v-row>
+                    </v-container>
 
-                <v-divider></v-divider>
+                    <v-list density="compact" nav>
+                        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" active></v-list-item>
+                        <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+                        <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-theme-provider>
+            <v-main>
+                <v-card color="grey-lighten-3">
+                    <v-layout>
+                        <v-app-bar
+                            color="teal-darken-4"
+                            image="https://picsum.photos/1920/1080?random"
+                        >
+                            <template v-slot:image>
+                                <v-img
+                                    gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+                                ></v-img>
+                            </template>
 
-                <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-            <v-app-bar
-                color="primary"
-                prominent
-            >
-                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                            <template v-slot:prepend>
+                                <v-app-bar-nav-icon @click.stop="rail = !rail"></v-app-bar-nav-icon>
+                            </template>
 
-                <v-toolbar-title>My files</v-toolbar-title>
+                            <v-app-bar-title>Title</v-app-bar-title>
 
-                <v-spacer></v-spacer>
+                            <v-spacer></v-spacer>
 
-                <v-btn variant="text" icon="mdi-magnify"></v-btn>
+                            <v-btn icon>
+                                <v-icon>mdi-power</v-icon>
+                            </v-btn>
+                        </v-app-bar>
 
-                <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-                <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-            </v-app-bar>
-            <v-main style="height: 95vh;">
-                <v-card title="Página de exemplo" color="yellow" height="80">
-                    <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']" style="padding-top: 0"></v-breadcrumbs>
+                        <v-main>
+                            <v-card title="Página de exemplo" color="white" height="80" :rounded="0">
+                                <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']" style="padding-top: 0"></v-breadcrumbs>
+                            </v-card>
+                            <v-container fluid>
+                                <v-row dense>
+                                    <v-col
+                                        v-for="n in 4"
+                                        :key="n"
+                                        cols="12"
+                                    >
+                                        <v-card
+                                            :title="`Content ${n}`"
+                                            :subtitle="`Subtitle for Content ${n}`"
+                                            text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+                                        ></v-card>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-main>
+                    </v-layout>
                 </v-card>
-                <v-card-text style="padding: 20px; background-color: antiquewhite" >
-                    The navigation drawer will appear from the bottom on smaller size screens.
-                </v-card-text>
             </v-main>
         </v-layout>
     </v-card>
@@ -68,7 +106,7 @@ export default {
                 { title: 'My Account', icon: 'mdi-account' },
                 { title: 'Users', icon: 'mdi-account-group-outline' },
             ],
-            rail: true,
+            rail: false,
         }
     },
 }
